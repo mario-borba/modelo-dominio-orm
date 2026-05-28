9823cfe9c9da4c6f2049370de6840eaa4bfc56ec
Mario <mariobielborba@hotmail.com>
2026-05-28 09:20:47 -0300

    pageble: buscar itens paginados

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,13 @@ public class ProductService {
     public ProductDTO findById(Long id) {
 
         return new ProductDTO(repository.findById(id).get());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+
+        Page<Product> listProducts = repository.findAll(pageable);
+
+        return listProducts.map(x -> new ProductDTO(x));
     }
 }
